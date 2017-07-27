c51352bb75b95a7ec9eaff7d5e130238ed9814c2
Ratnesh Kushwaha <ratnesh.kush@yahoo.in>
2017-07-27 12:11:40 +0530

    Compiled scala files

---
+++ MyDoc.java
@@ -0,0 +1,14 @@
+
+public class MyDoc
+{
+public String hello1( String s)
+{
+return s;
+} 
+
+public static void main (String args[])
+{
+MyDoc m= new MyDoc();
+System.out.println(m.hello1("Hello Ratnesh"));
+}
+}
---
+++ WordCount.scala
@@ -0,0 +1,11 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+
+object WordCount{
+def main(args: Array[String]){
+val sc = new SparkContext(args(0), "BasicMap", System.getenv("SPARK_HOME"))
+val file = sc.textFile(args(1)) 
+val words = file.flatMap(_.split(" ")).map((_, 1)).reduceByKey(_ +  _)
+words.saveAsTextFile(args(2))
+}
+}
